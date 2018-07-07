d8af5bf5edb5021c9310b6181939dbb8968dc314
Unknown <lau.reichardt@gmail.com>
2018-07-07 23:38:21 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+
+import lightBlue from '@material-ui/core/colors/lightBlue';
 import cyan from '@material-ui/core/colors/cyan';
 import teal from '@material-ui/core/colors/teal';
+
+import green from '@material-ui/core/colors/green';
+import lightGreen from '@material-ui/core/colors/lightGreen';
+import lime from '@material-ui/core/colors/lime';
 
 const elStyles = theme => ({
   root: {
     height: '100%',
-    width: '96px',
+    width: theme.spacing.unit * 12,
+    padding: theme.spacing.unit,
+  },
+  inner: {
+    backgroundColor: lightGreen[600],
+    width: '100%',
+    height: '100%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -17,7 +29,9 @@ const elStyles = theme => ({
 
 const unstyledElement = ({ classes, value }) => (
   <div className={classes.root}>
-    <Typography variant="display3">{value}</Typography>
+    <div className={classes.inner}>
+      <Typography variant="display3">{value}</Typography>
+    </div>
   </div>
 );
 
@@ -32,10 +46,16 @@ const styles = theme => ({
     marginLeft: 0,
   },
   laneContainer: {
-    width: '100%',
-    height: theme.spacing.unit * 24,
+    // width: '100%',
+    width: theme.spacing.unit * 120,
+    height: theme.spacing.unit * 36,
     display: 'flex',
     flexDirection: 'column',
+  },
+  lightBlue: {
+    flex: 1,
+    display: 'flex',
+    // backgroundColor: lightBlue[600],
   },
   cyan: {
     flex: 1,
@@ -49,33 +69,89 @@ const styles = theme => ({
   },
 });
 
-const arr = [1, 2, 3];
-const arr2 = [4, 5, 6, 7];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+Object.freeze(arr);
 
-const App = ({ classes }) => (
-  <div className={classes.App}>
-    <Typography variant="title" gutterBottom>
-      Job Shop Problem
-    </Typography>
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    <Button className={classes.button} variant="contained" color="primary">
-      Shuffle
-    </Button>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lane1: arr, lane2: [], lane3: [] };
+  }
 
-    <Button className={classes.button} variant="contained" color="primary">
-      Reset
-    </Button>
+  handleShuffle = () => {
+    this.setState(prev => {
+      const temp = arr.slice();
 
-    <div className={classes.laneContainer}>
-      <div className={classes.cyan}>
-        {arr.map(num => <Element key={num.toString()} value={num} />)}
+      temp.sort(function() {
+        return 0.5 - Math.random();
+      });
+
+      const rand = getRandomInt(1, 8);
+
+      return {
+        lane1: [],
+        lane2: temp.slice(0, rand),
+        lane3: temp.slice(rand, 9),
+      };
+    });
+  };
+
+  handleReset = () => {
+    this.setState({ lane1: arr, lane2: [], lane3: [] });
+  };
+
+  render() {
+    const classes = this.props.classes;
+    return (
+      <div className={classes.App}>
+        <Typography variant="title" gutterBottom>
+          Job Shop Problem
+        </Typography>
+
+        <Button
+          onClick={this.handleShuffle}
+          className={classes.button}
+          variant="contained"
+          color="primary"
+        >
+          Shuffle
+        </Button>
+
+        <Button
+          onClick={this.handleReset}
+          className={classes.button}
+          variant="contained"
+          color="primary"
+        >
+          Reset
+        </Button>
+
+        <div className={classes.laneContainer}>
+          <div className={classes.lightBlue}>
+            {this.state.lane1.map(num => (
+              <Element key={num.toString()} value={num} />
+            ))}
+          </div>
+
+          <div className={classes.cyan}>
+            {this.state.lane2.map(num => (
+              <Element key={num.toString()} value={num} />
+            ))}
+          </div>
+
+          <div className={classes.teal}>
+            {this.state.lane3.map(num => (
+              <Element key={num.toString()} value={num} />
+            ))}
+          </div>
+        </div>
       </div>
-
-      <div className={classes.teal}>
-        {arr2.map(num => <Element key={num.toString()} value={num} />)}
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default withStyles(styles)(App);
