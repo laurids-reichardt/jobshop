dad70e23406175f8675138b782b9775716d371bc
Unknown <lau.reichardt@gmail.com>
2018-07-07 18:12:25 +0200

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Typography gutterBottom>none</Typography>
+        <Typography variant="body1" gutterBottom>
+          body1
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          body2
+        </Typography>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <React.Fragment>
+    <CssBaseline />
+    <App />
+  </React.Fragment>,
+  document.getElementById('root')
+);
