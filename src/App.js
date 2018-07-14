9a5e57243309ad52600d2aa8ed25122990b93a8e
Unknown <timo.klinz@googlemail.com>
2018-07-14 16:56:03 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function genJobArray(jobs, machines, maxIntervalLength) {
     let job = [];
     // for every job create a pair of machine & interval and push to array
     for (let j = 0; j < machines; j++) {
-      job.push({ machine: j, interval: getRandomInt(1, maxIntervalLength) });
+      job.push({ machine: j, interval: getRandomInt(1, maxIntervalLength), earliestStart: 0});
     }
     // shuffle machine lane order and push to matrix array
     matrix.push(shuffle(job));
@@ -73,35 +73,107 @@ function genMachineOrder(jobMatrix, machinesAmount, jobsAmount) {
   return machineMatrix;
 }
 
-function genMachineOrderForGant(machineMatrix, jobMatrix, machinesAmount, jobsAmount){
-  console.log("Hallo");
-  console.log(jobMatrix);
+function genMachineOrderForGant(machineMatrix, jobMatrix2, machinesAmount, jobsAmount){
+  
+  //Diese 2 MaschinenMatrix wird benötigt, da wir sonst die zufällig erstellte 
+  //Maschinen Liste löschen. Daher muss mit der komischen copy Methode eine "deep copy" 
+  //erstellt werden. 
+  let machineMatrix2 = copy(machineMatrix);
 
+  //Initialising gantMatrix
   const gantMatrix = [];
+    for(let index = 0; index < machinesAmount; index++){
+      gantMatrix.push([]);
+    }
+  
+  //da alle Tasks in die GantMatix eingetragen werden müssen, müssen alle Einträge der Matrix 
+  //durchlaufen werden. 
+    for(let wholeMatrix = 0; wholeMatrix < machinesAmount * jobsAmount; wholeMatrix ++){
+      //Es wird hier geschaut, ob bei Maschine 1 oder 2 der nächste abzuarbeitende Index ist.
+      for(let machineIndex = 0; machineIndex < machinesAmount; machineIndex++){
+        //Wenn die Zeile (s. 5 weiter unten "machineMatrix2[machineIndex].shift();") schon
+        //beim letzten Element aus der MaschinenListe ausgeführt wurde, dann findet er kein
+        //Object mehr und es kommt eine Fehlermeldung. Daher ===undefinded Abfrage. 
+        if(machineMatrix2[machineIndex][0]===undefined){
+        }else{
+          //Wenn der Index passt -> Eintrag aus der MaschinenListe löschen 
+          //                     -> Eintrag in GantMatrix
+          if(machineMatrix2[machineIndex][0].index === wholeMatrix){
+            let nextJobNumber = machineMatrix2[machineIndex][0].job;
+            machineMatrix2[machineIndex].shift();
+            console.log(nextJobNumber);
+            insertJobInGant(gantMatrix, jobMatrix2, nextJobNumber);
+          }
+        }
+      }
+    }
+  console.log("JobMatrix2: ");
+  console.log(jobMatrix2);
+  console.log("machineMatrix ");
+  console.log(machineMatrix);
+  console.log("Abgearbeitete machineMatrix2:");
+  console.log(machineMatrix2);
+  return gantMatrix;
+}
 
-  for(let index = 0; index < machinesAmount; index++){
-    gantMatrix.push([]);
+function insertJobInGant(gantMatrix, jobMatrix2, nextJobNumber){
+  
+  let currentTaskObject = null;
+  let nextTaskObject = null;
+  let earliestStartCurrentTask = null;
+  let earliestStartNextTask = null;
+  let machineArrayLength = null;
+  
+  //maschineBestimmen anhand Job
+  //aktuelleLängeDerMaschineBestimmen
+
+  //überschreiben des nächsten Startwertes und löschen des 1. Tasks 
+  if(jobMatrix2[nextJobNumber][1]===undefined){
+
+  }else{
+
+    currentTaskObject = jobMatrix2[nextJobNumber][0]; 
+    nextTaskObject = jobMatrix2[nextJobNumber][1];
+    earliestStartCurrentTask = jobMatrix2[nextJobNumber][0].earliestStart;
+    earliestStartNextTask = jobMatrix2[nextJobNumber][1].earliestStart;
+    machineArrayLength = gantMatrix[jobMatrix2[nextJobNumber][0].machine].length;
+
+    // console.log(currentTaskObject + " " + nextTaskObject + " " + earliestStartCurrentTask + " " + earliestStartNextTask);
+    // console.log(jobMatrix2[nextJobNumber][0] + " " + jobMatrix2[nextJobNumber][1] + " " + jobMatrix2[nextJobNumber][0].earliestStart + " " + jobMatrix2[nextJobNumber][1].earliestStart);
+    earliestStartNextTask = earliestStartCurrentTask + currentTaskObject.interval;
+    console.log("Überschriebener earliestStartWert: "+ earliestStartNextTask);
+    console.log(jobMatrix2[nextJobNumber][1].earliestStart);
   }
+  if(currentTaskObject===undefined){
 
-  console.log(machinesAmount);
+  }else{
+      if(earliestStartCurrentTask - machineArrayLength > 0){
+        for(let index = machineArrayLength; index <= earliestStartCurrentTask; index++){
+          gantMatrix[currentTaskObject.machine].push({jobNummer: -1});
+        }
+      }else{
+          for(let index = earliestStartCurrentTask; index < machineArrayLength; index ++){
+            let amountFreeTimeSlot = 0; 
+            if(gantMatrix[index].jobNummer === -1){
+              amountFreeTimeSlot++;
+              if(amountFreeTimeSlot === jobMatrix2[nextJobNumber][0].interval){
+                //eintragen
+              }
+            }
+          }
+        }
+      jobMatrix2[nextJobNumber].shift();
+    }
+}
 
-  for(let index = 0; index < machinesAmount * jobsAmount; index ++){
-    let machineEntry = null;
-    let counter = 0;
-    machineEntry = machineMatrix[1].shift();
-    let jobNumber = machineEntry.job;
-    console.log("Hallo2");
-    console.log(machineEntry);
-    console.log("jobNumber " + jobNumber);
-
-    do{
-      counter ++;
-      console.log(counter);
-    }while(counter < 10);
+function copy(o) {
+  var output, v, key;
+  output = Array.isArray(o) ? [] : {};
+  for (key in o) {
+      v = o[key];
+      output[key] = (typeof v === "object") ? copy(v) : v;
   }
-
-
-  return null;
+  return output;
 }
 
 
@@ -134,8 +206,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      machines: 2,
-      jobs: 3,
+      machines: 3,
+      jobs: 1,
       variants: 100,
       maxInterval: 5,
       jobsStr: 'Test',
@@ -147,11 +219,19 @@ class App extends React.Component {
   handleGenerate = () => {
     this.setState(
       prev => {
-        const jobMatrix = genJobArray(
+        const jobMatrixOriginal = genJobArray(
           prev.jobs,
           prev.machines,
           prev.maxInterval
         );
+
+        console.log("JobMatrix Original: ");
+        console.log(jobMatrixOriginal);
+        
+        //Die JobMatrixen werden häufiger benötigt. Daher copy Methoden Aufruf 
+        const jobMatrix = copy(jobMatrixOriginal);
+        const jobMatrix2 = copy(jobMatrixOriginal);
+
         const jobStr = genJobStringMatrix(jobMatrix);
         const machineMatrix = genMachineOrder(
           jobMatrix,
@@ -161,7 +241,7 @@ class App extends React.Component {
 
         const gantMatrix = genMachineOrderForGant(
           machineMatrix, 
-          jobMatrix, 
+          jobMatrix2, 
           this.state.machines, 
           this.state.jobs
         );
